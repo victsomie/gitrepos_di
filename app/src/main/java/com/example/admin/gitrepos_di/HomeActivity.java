f78bd8ca1b87932388e1398ea25f074947311136
victsomie <victsomie@yahoo.com>
2018-05-07 19:27:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/admin/gitrepos_di/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.gitrepos_di;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
